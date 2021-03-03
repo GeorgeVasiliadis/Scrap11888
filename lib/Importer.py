c6f0baf6cbb4e16900e3041065a3a8a078d921c8
George Vasiliadis <gvasiliadis2@gmail.com>
2021-03-03 23:29:52 +0200

    Added Importer class

---
+++ lib/Importer.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+def importNames(filename, logger):
+    names = []
+
+    try:
+        workbook = openpyxl.load_workbook(filename)
+        sheet = workbook.active
+        for i in range(1,sheet.max_row+1):
+            names.append(sheet.cell(row=i, column=1).value)
+    except:
+        logger.log("An error occured while trying to process a file.", "error")
+    return names
